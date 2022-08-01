decedb82bf45170c599582417e9ea04b066123d9
sayronaught <sayro_naught@hotmail.com>
2022-08-01 19:38:11 +0200

    test rocks & hand movement

---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/MultiplayerPhoton/Lobby/SJRoomController.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class SJRoomController : MonoBehaviourPunCallbacks
+{
+
+    public override void OnEnable()
+    {
+        //base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+    public override void OnDisable()
+    {
+        //base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+        Debug.Log("Joined room");
+        StartGame();
+    }
+    void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting game");
+            //PhotonNetwork.LoadLevel(MultiplayerSceneIndex);
+        }
+    }
+}
