05cc0b23193c764642a2395c19bef0f1fc6daafd
sayronaught <sayro_naught@hotmail.com>
2022-08-01 15:51:23 +0200

    same scene multiplayer

---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/MultiplayerPhoton/Lobby/JSRoomController.cs
@@ -0,0 +1,33 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class JSRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int MultiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        //base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+    public override void OnDisable()
+    {
+        //base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+        Debug.Log("Joined room");
+        StartGame();
+    }
+    void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting game");
+            //PhotonNetwork.LoadLevel(MultiplayerSceneIndex);
+        }
+    }
+}
---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/MultiplayerPhoton/Lobby/SJLobbyController.cs
@@ -0,0 +1,56 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SJLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int roomSize;
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        PhotonNetwork.AutomaticallySyncScene = true;
+        //QuickStartButton.SetActive(true);
+        QuickStart();
+    }
+
+    public void QuickStart()
+    {
+        //QuickStartButton.SetActive(false);
+        //QuickStartCancel.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Quick Start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        //base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+    void CreateRoom()
+    {
+        Debug.Log("Creating room now");
+        int RandomRoomNumber = Random.Range(0, 10000);
+        RoomOptions RoomOp = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
+        PhotonNetwork.CreateRoom("QuickRoom " + RandomRoomNumber, RoomOp);
+        Debug.Log("Room " + RandomRoomNumber);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        //base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log("Failed creating room, trying again");
+        CreateRoom();
+    }
+
+    public void QuickCancel()
+    {
+        //QuickStartButton.SetActive(true);
+        //QuickStartCancel.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+        Debug.Log("Leaving Room");
+    }
+}
