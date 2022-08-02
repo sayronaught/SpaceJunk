6c86af3f4810a9b316537ab2a17ad73e1a1aae89
sayronaught <sayro_naught@hotmail.com>
2022-08-02 08:43:50 +0200

    Stick cockpit controls

--- SpaceJunk/Assets/_SpaceJunk/Scripts/Cockpit.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/Cockpit.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class Cockpit : MonoBehaviour
 {
     List<UnityEngine.XR.InputDevice> leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+    List<UnityEngine.XR.InputDevice> rightHandDevices = new List<UnityEngine.XR.InputDevice>();
 
     bool leftTrigger = false;
+    Vector2 leftStick;
+    Vector2 rightStick;
 
     private PhotonView myPV;
 
@@ -18,24 +21,61 @@ public class Cockpit : MonoBehaviour
     }
 
     [PunRPC]
-    public void moveShip(Vector3 pos)
+    public void moveShip(Vector3 pos,Quaternion rot)
     {
         transform.position = pos;
+        transform.rotation = rot;
+    }
+
+    void updateAllEntities()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
         if ( leftHandDevices.Count > 0)
         {
             leftHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out leftTrigger);
+            leftHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out leftStick);
+        }
+        if (rightHandDevices.Count > 0)
+        {
+            //leftHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out leftTrigger);
+            rightHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out rightStick);
         }
         if (leftTrigger)
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, Time.deltaTime);
-            Debug.Log("Trigger button is pressed.");
-            myPV.RPC("moveShip", RpcTarget.All, transform.position);
+            //Debug.Log("Trigger button is pressed.");
+            myPV.RPC("moveShip", RpcTarget.All,transform.position,transform.rotation);
+        }
+        if (leftStick.x != 0f)
+        {
+            transform.Rotate(transform.forward, leftStick.x * Time.deltaTime *-25f);
+            myPV.RPC("moveShip", RpcTarget.All, transform.position, transform.rotation);
+        }
+        if (leftStick.y != 0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (transform.forward*leftStick.y), Time.deltaTime*10f);
+            myPV.RPC("moveShip", RpcTarget.All, transform.position, transform.rotation);
+        }
+        if (rightStick.x != 0f)
+        {
+            transform.Rotate(transform.up, rightStick.x * Time.deltaTime * 25f);
+            myPV.RPC("moveShip", RpcTarget.All, transform.position, transform.rotation);
+        }
+        if (rightStick.y != 0f)
+        {
+            transform.Rotate(transform.right, rightStick.y * Time.deltaTime * -25f);
+            myPV.RPC("moveShip", RpcTarget.All, transform.position, transform.rotation);
+        }
+        if (PhotonNetwork.IsMasterClient)
+        {
+            updateAllEntities();
         }
     }
 }
