e66c0318e6c6c6be01b5d6ea8394dbaee6e16bd0
sayronaught <sayro_naught@hotmail.com>
2022-08-09 23:43:02 +0200

    velocity meter I hope
    cant test before new cockpit / rigidbody script is in place. but we are getting close to that now..

--- SpaceJunk/Assets/_SpaceJunk/Scripts/UI/ShipSpeed.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/UI/ShipSpeed.cs
@@ -1,29 +1,30 @@
-using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
-public class PlayerCount : MonoBehaviour
+public class ShipSpeed : MonoBehaviour
 {
 
     private float updateTimer = 0f;
 
     private TMP_Text myTxt;
 
+    private Rigidbody shipRB;
+
     // Start is called before the first frame update
     void Start()
     {
         myTxt = GetComponent<TMP_Text>();
+        shipRB = GameObject.Find("PlayerShip").GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( updateTimer < 0f )
+        if (updateTimer < 0f)
         {
-            myTxt.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
+            myTxt.text = shipRB.velocity.magnitude.ToString()+ " m/s";
             updateTimer = 1f;
         }
         updateTimer -= Time.deltaTime;
