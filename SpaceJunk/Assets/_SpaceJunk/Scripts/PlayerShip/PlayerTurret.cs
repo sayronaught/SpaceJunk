fdeee4db65ff9a325ca91ba89aa64de44a2f8930
sayronaught <sayro_naught@hotmail.com>
2022-08-13 13:48:16 +0200

    Update PlayerTurret.cs

--- SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerTurret : MonoBehaviour
 {
+    public Transform turretSwivel;
+    public Transform gunMount;
+
+    private PlayerStation MyStation;
+
+    private Vector3 turretDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MyStation = GetComponent<PlayerStation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( MyStation.thisPlayer != null)
+        { // current player is seated in this turret right now
+            turretDirection = transform.position - ((MyStation.thisPlayer.leftController.transform.forward + MyStation.thisPlayer.rightController.transform.forward)*0.5f);
+            turretSwivel.rotation = Quaternion.Euler(0f,turretDirection.y, 0f);
+            gunMount.rotation = Quaternion.Euler(turretDirection.x, 0f, 0f);
+        }
     }
 }
