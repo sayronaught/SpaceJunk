c0bcbdf7cafe5c3c14d25fc8d88a02b7e57d1b90
sayronaught <sayro_naught@hotmail.com>
2022-08-13 18:34:51 +0200

    crappy shots

--- SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
@@ -6,10 +6,14 @@ public class PlayerTurret : MonoBehaviour
 {
     public Transform turretSwivel;
     public Transform[] barrelEnds;
+    private int currentBarrel = 0;
+    private float shootDelay = 0f;
+    public GameObject AmmoPrefab;
 
     public float maxDegreesPerSecond = 35f;
 
     private PlayerStation MyStation;
+    private AudioSource myAS;
 
     private Quaternion aim;
 
@@ -17,6 +21,7 @@ public class PlayerTurret : MonoBehaviour
     void Start()
     {
         MyStation = GetComponent<PlayerStation>();
+        myAS = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +31,15 @@ public class PlayerTurret : MonoBehaviour
         { // current player is seated in this turret right now
             turretSwivel.rotation = Quaternion.Lerp(MyStation.thisPlayer.leftController.transform.rotation, MyStation.thisPlayer.rightController.transform.rotation, 0.5f);
             turretSwivel.Rotate(Vector3.left, -90);
+            shootDelay -= Time.deltaTime;
+            if ( shootDelay < 0f && (MyStation.thisPlayer.playerLeftTrigger || MyStation.thisPlayer.playerRightTrigger))
+            {
+                var shot = Instantiate(AmmoPrefab, barrelEnds[currentBarrel].position, barrelEnds[currentBarrel].rotation);
+                shot.GetComponent<Rigidbody>().AddForce(barrelEnds[currentBarrel].forward * 10000f);
+                Destroy(shot, 3000);
+                myAS.Play();
+                shootDelay = 0.5f;
+            }
         }
     }
 }
