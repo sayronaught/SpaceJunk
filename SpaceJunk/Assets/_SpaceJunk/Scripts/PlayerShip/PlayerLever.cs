f04b94d72e3bb074dacbe421c981735ee86ee71a
sayronaught <sayro_naught@hotmail.com>
2022-08-14 08:14:25 +0200

    cockpit lever structural

---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerLever.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLever : MonoBehaviour
+{
+    public BoxCollider leverUp;
+    public BoxCollider leverGrab;
+    public BoxCollider leverDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+/*
+if(hitToTest.collider.bounds.Contains(telePosition))
+{
+   print("point is inside collider");
+}
+*/
+    }
+}
