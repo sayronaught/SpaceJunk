77cf2973ffd780a626938d91b8c5f4cc448ced29
sayronaught <sayro_naught@hotmail.com>
2022-08-21 09:38:40 +0200

    ship network movement
    extremely crude and alpha-ish.. probably many alpha tests from smooth and coordinated movement, but were getting closer. the logic isn't far off

--- SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerShip.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerShip.cs
@@ -22,21 +22,51 @@ public class PlayerShip : MonoBehaviour
     public Vector2 controlsLeft;
     public Vector2 controlsRight;
 
+    // non hosts, needs to know hwere to move ship towards
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+    private float updateTimer = 0f;
+    private PhotonView myPV;
+    private Rigidbody myRB;
+
     [PunRPC]
     public void sendCockpitControlSpeed(int changeSpeed)
     {
         controlSpeedStage = changeSpeed;
     }
 
+    [PunRPC]
+    public void updateShipFromHost(Vector3 targetPos,Quaternion targetRot, Vector3 velocity, Vector3 rotation)
+    { // host sends position and movement to other ships
+        targetPosition = targetPos;
+        targetRotation = targetRot;
+        myRB.velocity = velocity;
+        myRB.angularVelocity = rotation;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myPV = GetComponent<PhotonView>();
+        myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PhotonNetwork.IsMasterClient)
+        { // host sends ship updates
+            if ( updateTimer < 0 )
+            {
+                myPV.RPC("updateShipFromHost", RpcTarget.All, transform.position, transform.rotation,myRB.velocity,myRB.angularVelocity);
+                updateTimer = 0.2f;
+            }
+            updateTimer -= Time.deltaTime;
+        } else { // non hosts, gently move there
+            targetPosition += myRB.velocity;
+            targetRotation *= Quaternion.Euler(myRB.angularVelocity);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime);
+        }
     }
 }
