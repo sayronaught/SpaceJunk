c957f70061179b2bb29f4e69016c4403dc08af5e
sayronaught <sayro_naught@hotmail.com>
2022-08-21 13:19:58 +0200

    coordinate turrets

--- SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/PlayerShip/PlayerTurret.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,14 +15,41 @@ public class PlayerTurret : MonoBehaviour
 
     private PlayerStation MyStation;
     private AudioSource myAS;
+    private PhotonView myPV;
+    private float updateTimer = 0f;
 
     private Quaternion aim;
+
+    [PunRPC]
+    public void updateTurret(Quaternion swivel)
+    {
+        if (MyStation.thisPlayer == null)
+            turretSwivel.rotation = swivel;
+    }
+
+    [PunRPC]
+    public void FireTurret()
+    {
+        if (MyStation.thisPlayer == null)
+        {
+            fireTheTurret();
+        }
+    }
+
+    private void fireTheTurret()
+    {
+        var shot = Instantiate(AmmoPrefab, barrelEnds[currentBarrel].position, barrelEnds[currentBarrel].rotation);
+        shot.GetComponent<Rigidbody>().AddForce(barrelEnds[currentBarrel].forward * 10000f);
+        Destroy(shot, 3000);
+        myAS.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         MyStation = GetComponent<PlayerStation>();
         myAS = GetComponent<AudioSource>();
+        myPV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -31,13 +59,16 @@ public class PlayerTurret : MonoBehaviour
         { // current player is seated in this turret right now
             turretSwivel.rotation = Quaternion.Lerp(MyStation.thisPlayer.leftController.transform.rotation, MyStation.thisPlayer.rightController.transform.rotation, 0.5f);
             turretSwivel.Rotate(Vector3.left, -90);
+            if ( updateTimer < 0f )
+            {
+                myPV.RPC("updateTurret", RpcTarget.All, turretSwivel.rotation);
+                updateTimer = 0.2f;
+            }
+            updateTimer -= Time.deltaTime;
             shootDelay -= Time.deltaTime;
             if ( shootDelay < 0f && (MyStation.thisPlayer.playerLeftTrigger || MyStation.thisPlayer.playerRightTrigger))
             {
-                var shot = Instantiate(AmmoPrefab, barrelEnds[currentBarrel].position, barrelEnds[currentBarrel].rotation);
-                shot.GetComponent<Rigidbody>().AddForce(barrelEnds[currentBarrel].forward * 10000f);
-                Destroy(shot, 3000);
-                myAS.Play();
+                fireTheTurret();
                 shootDelay = 0.5f;
             }
         }
