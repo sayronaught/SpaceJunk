f1bfba8c4bc4cf4bdb27af4150705e6fd1b35b37
sayronaught <sayro_naught@hotmail.com>
2022-10-03 13:45:46 +0200

    animated ui with sound

--- SpaceJunk/Assets/_SpaceJunk/Scripts/UI/UXML/NoHeadSet/UiNoHeadSet_Main.cs
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/UI/UXML/NoHeadSet/UiNoHeadSet_Main.cs
@@ -5,22 +5,40 @@ using UnityEngine.UIElements;
 
 public class UiNoHeadSet_Main : MonoBehaviour
 {
+    public AudioClip sfxPiston;
+
     private float updateTimer = 0f;
 
     private VisualElement myRoot;
+    private AudioSource myAS;
 
     private Label labelSpeed;
     private Label labelMass;
     private Rigidbody shipRB;
     private float convert;
+    private VisualElement speedmasspanel;
+
+    private void playSfxPiston()
+    {
+        if (myAS.isPlaying) return;
+        myAS.clip = sfxPiston;
+        myAS.volume = 0.2f;
+        myAS.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         myRoot = GetComponent<UIDocument>().rootVisualElement;
         shipRB = GameObject.Find("PlayerShip").GetComponent<Rigidbody>();
+        myAS = GetComponent<AudioSource>();
         labelSpeed = myRoot.Q<Label>("Speed");
         labelMass = myRoot.Q<Label>("Mass");
+        speedmasspanel = myRoot.Q<VisualElement>("SpeedMass");
+        speedmasspanel.RegisterCallback<MouseOverEvent>((type) =>
+        {
+            playSfxPiston();
+        });
     }
 
     // Update is called once per frame
