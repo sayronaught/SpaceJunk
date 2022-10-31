b336864697e5f82b88d775d5b5b370000fc4a84c
sayronaught <sayro_naught@hotmail.com>
2022-10-31 14:20:14 +0100

    Split into shipmode & tournamentmode

---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/TournamentMode/gameManager/gameManagerTournament.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManagerTournament : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Physics.gravity = new Vector3(0f, -9.81f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
