a71acc8aef3d93b7a9d8e21035cfa787aaf8228e
sayronaught <sayro_naught@hotmail.com>
2022-11-08 14:29:18 +0100

    db_user

---
+++ SpaceJunk/Assets/_SpaceJunk/Scripts/Lobby/db_data/db_user.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class db_user 
+{
+    // user ID in the database
+    public int id;
+    // username
+    public string username;
+}
